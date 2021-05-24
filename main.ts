37f0d62ab17afd22f28ac5fc45cf8ed6d0c54341
puchm <moritz.puchinger@gmail.com>
2021-05-24 19:44:34 +0200

    Simplify example

--- main.ts
+++ main.ts
@@ -11,11 +11,4 @@ console.log('running');
 
 const p = new Promise((resolve, reject) => {
     setTimeout(resolve, 10000);
-});
-
-async function start() {
-    await p;
-    console.log('Promise resolved');
-}
-
-start();
+}).then(() => console.log('Promise resolved'))
